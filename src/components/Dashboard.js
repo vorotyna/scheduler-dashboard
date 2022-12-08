505b1254948e5548d9f6d6f62f5d848c96862741
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-12-08 13:24:33 -0800

    Bind this

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -31,26 +31,38 @@ const data = [
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
 
+  selectPanel(id) {
+    this.setState(prev => ({
+      focused: prev.focused !== null ? null : id
+    }));
+  }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map(panel => (
-      <Panel
-        key={panel.id}
-        id={panel.id}
-        label={panel.label}
-        value={panel.value}
-      />
-    ));
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={event => this.selectPanel(panel.id)}
+        />
+      ));
 
-    return <main className={dashboardClasses}>{panels}</main>;
+    return (
+      <main className={dashboardClasses}>{panels}</main>
+    );
   }
 }
 
